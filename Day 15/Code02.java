fe62d333b8231e10df8821643079d57aa851ea7a
sandesh <sandeshchapagain99@gmail.com>
2026-01-16 22:20:06 +0545

    Day 15 code 01 and 02

--- Day 15/Code02.java
+++ Day 15/Code02.java
@@ -1,2 +1,21 @@
 public class Code02 {
+    public static void main(String[] args){
+        int [] arr = { 1,2,2,3,3,3,4,4,9,10,10};
+        boolean isFirst = false;
+        System.out.println("First occurrence of each unique element:");
+        for(int i =0; i<arr.length; i++){
+            // Check if this element appears before current position
+            isFirst = true;
+            for(int j =0; j<i; j++){
+                    if(arr[i] == arr[j]){
+                        isFirst = false;
+                        break;
+                    }
+            }
+            // If it's the first occurrence, print it
+            if(isFirst){
+                System.out.println("arr["+i+"] : "+arr[i]);
+            }
+        }
+    }
 }

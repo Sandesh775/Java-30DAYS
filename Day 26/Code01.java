a82ff483ac655106822baec29a41eac7a05465a2
Sharwan jung kunwar <sharwankunwar07@gmail.com>
2026-02-14 16:56:50 +0545

    Fix nested loop index to avoid duplicate pairs

--- Day 26/Code01.java
+++ Day 26/Code01.java
@@ -12,7 +12,7 @@ public class Code01 {
         System.out.println("Now enter target number to form from this array :");
         int target = sc.nextInt();
         for(int i = 0; i<a.length; i++){
-            for( int j = 1; j<a.length; j++){
+            for( int j = i+1; j<a.length; j++){
                 if((a[i]+a[j] == target)){
                     System.out.println("Output : "+a[i]+" + "+a[j]);
                     return;

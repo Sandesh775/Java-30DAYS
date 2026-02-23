0fe405f62fb8e0b03fbb35b6d4fd3b6641f4af50
sandesh <sandeshchapagain99@gmail.com>
2026-02-23 17:50:57 +0545

    Day 29 completed !

--- Day 29/Code06.java
+++ Day 29/Code06.java
@@ -2,6 +2,13 @@ public class Code06 {
     public static void main(String[] args) {
         int[] a = {1,2,5,3,6,7};
         int[] b = {7,9,1,3};
-
+        System.out.println("Intersection elements of two arrays : ");
+        for ( int i = 0; i<a.length; i++){
+            for( int j = 0; j<b.length; j++){
+                if(a[i] == b[j]){
+                    System.out.print(a[i]+" ");
+                }
+            }
+        }
     }
 }

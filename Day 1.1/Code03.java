adf67de1435abd5699994b957fddc9c59b09d00c
sandesh <sandeshchapagain99@gmail.com>
2026-03-20 00:07:28 +0545

    Equal Sum Split

--- Day 1.1/Code03.java
+++ Day 1.1/Code03.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /*
 *  * Question 3: Given an array, calculate prefix sum array
 * Concept : Prefix Sum
@@ -5,16 +7,31 @@
  * Output: {1, 3, 6, 10, 15}
  * prefix[i] = sum of arr[0] to arr[i]
  * */
+// This is the most optimal one !
 public class Code03 {
     public static void main(String[] args) {
         int[] a = {1, 2, 3, 4, 5};
-        int sum = 0;
-        for (int i = 0; i < a.length; i++) {
-            sum += a[i];   // incremental accumulation
-            System.out.print(sum + " ");
+        int[] prefix = new int[a.length];
+        prefix[0] = a[0];
+        for( int i = 1; i<a.length; i++){
+            prefix[i] += prefix[i-1]+a[i];
+        }
+        System.out.println("Prefix sum array ! : ");
+        for(int i : a){
+            System.out.print(i+" ");
         }
     }
 }
+//public class Code03 {
+//    public static void main(String[] args) {
+//        int[] a = {1, 2, 3, 4, 5};
+//        int sum = 0;
+//        for (int i = 0; i < a.length; i++) {
+//            sum += a[i];   // incremental accumulation
+//            System.out.print(sum + " ");
+//        }
+//    }
+//}
 /*
 public class Code03 {
     public static void main(String[] args) {
